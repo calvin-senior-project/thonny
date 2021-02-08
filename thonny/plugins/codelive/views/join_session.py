00cdc87058ce035c68f08b6b2c88b97817531cc1
samigirma1 <sgz4>
2021-02-08 01:16:07 -0500

    Style improvements for join_session dialog

--- thonny/plugins/codelive/views/join_session.py
+++ thonny/plugins/codelive/views/join_session.py
@@ -1,27 +1,27 @@
 import tkinter as tk
-
+from tkinter import ttk
 from thonny.plugins.codelive.mqtt_connection import topic_exists
 
-BG = "#EEEEDD"
+BG = "#EEEEE4"
 class JoinSessionDialog(tk.Toplevel):
-    def __init__(self, master):
-        tk.Toplevel.__init__(self, master, bg = BG)
+    def __init__(self, parent):
+        tk.Toplevel.__init__(self, parent, bg = BG)
+        self.protocol("WM_DELETE_WINDOW", self.cancel_callback)
+        self.title("Create Live Session")
         self.data = dict()
 
-        Intro = tk.Label(self, 
-                        text="Please Provide information needed to start your new CodeLive Session.",
-                        bg = BG)
+        frame = ttk.Frame(self)
         
-        form_frame = tk.Frame(self, bg = BG)
+        intro = ttk.Label(frame, 
+                        text="Please Provide information needed to join an existing CodeLive Session.")
+        
+        form_frame = ttk.Frame(frame)
 
-        name_label = tk.Label(form_frame, text = "Your alias", bg = BG)
-        self.name_input = tk.Text(form_frame, height= 1, bd = 5)
+        name_label = ttk.Label(form_frame, text = "Your alias")
+        self.name_input = tk.Text(form_frame, height= 1)
 
-        session_topic_label = tk.Label(form_frame, text = "Session Topic", bg = BG)
-        self.topic_input = tk.Text(form_frame, height= 1, bd = 5)
-
-        self.name_input.bind("<KeyPress>", self.allow_start_name, True)
-        self.topic_input.bind("<KeyPress>", self.allow_start_topic, True)
+        session_topic_label = ttk.Label(form_frame, text = "Session Topic")
+        self.topic_input = tk.Text(form_frame, height= 1)
 
         name_label.grid(row = 0, column = 0, sticky = tk.E)
         self.name_input.grid(row = 0, column = 1, sticky = tk.W, padx = 10, pady = 5)
@@ -29,72 +29,83 @@ class JoinSessionDialog(tk.Toplevel):
         session_topic_label.grid(row = 1, column = 0, sticky=tk.E)
         self.topic_input.grid(row = 1, column = 1, sticky=tk.W, padx = 10, pady = 5)
         
-        button_frame = tk.Frame(self, bg = BG)
+        button_frame = ttk.Frame(frame)
 
-        self.start_button = tk.Button(button_frame,
+        start_button = tk.Button(button_frame,
                                   text="Join!",
-                                  command=self.cancel_callback,
-                                  fg = "green")
-        self.start_button["state"] = tk.DISABLED
+                                  command=self.join_callback,
+                                  fg = "green",
+                                  width = 10)
 
         cancel_button = tk.Button(button_frame,
                                   text="Cancel",
                                   command=self.cancel_callback,
-                                  fg = "red")
+                                  fg = "red",
+                                  width = 10)
         
-        self.start_button.pack(side=tk.RIGHT, fill=tk.X, expand=True)
-        cancel_button.pack(side=tk.LEFT, fill=tk.X, expand=True)
+        start_button.pack(side=tk.RIGHT, padx = 5)
+        cancel_button.pack(side=tk.LEFT, padx = 5)
 
-        Intro.pack(expand=True, padx = 10, pady = 5)
+        intro.pack(expand=True, padx = 10, pady = 5)
         form_frame.pack(fill=tk.X, expand=True, padx = 10, pady=5)
-        button_frame.pack(fill=tk.X, expand=True, padx = 10, pady=5)
+        button_frame.pack(side = tk.BOTTOM, padx = 10, pady=5)
 
-    def allow_start_name(self, event):
-        name = self.name_input.get("0.0", "end")
-        topic = self.topic_input.get("0.0", "end")
+        self.center(parent.winfo_geometry())
 
-        if len(topic.strip()) == 0:
-            print("disabling")
-            self.start_button["state"] = tk.DISABLED
-        else:
-            if event.keysym == "BackSpace" and len(name.strip()) == 1:
-                print("disabling")
-                self.start_button["state"] = tk.DISABLED
-            else:
-                print("enabling")
-                self.start_button["state"] = tk.NORMAL
-    
-    def allow_start_topic(self, event):
-        name = self.name_input.get("0.0", "end")
-        topic = self.topic_input.get("0.0", "end")
+    def center(self, parent_geo):
+        parent_dim, parent_x, parent_y = parent_geo.split("+")
+        parent_w, parent_h = [int(l) for l in parent_dim.split("x")]
 
-        if len(name.strip()) == 0:
-            print("disabling")
-            self.start_button["state"] = tk.DISABLED
-        
-        else:
-            if event.keysym == "BackSpace" and len(topic.strip()) == 1:
-                print("disabling")
-                self.start_button["state"] = tk.DISABLED
-            else:
-                print("enabling")
-                self.start_button["state"] = tk.NORMAL
+        parent_x = int(parent_x)
+        parent_y = int(parent_y)
 
-    def start_callback(self):
+        w = 400
+        h = 200
+
+        x = parent_x + (parent_w - w) / 2
+        y = parent_y + (parent_h - h) / 2
+
+        self.geometry("%dx%d+%d+%d" % (w, h, x, y))
+
+    def join_callback(self):
         name = self.name_input.get("0.0", "end").strip()
         topic = self.topic_input.get("0.0", "end").strip()
 
-        # TODO: if topic doesn't exist, raise warning...
-        if True: #topic_exists(topic):
+        if self.valid_name(name) and self.valid_topic(name):
             self.data["name"] = name
             self.data["topic"] = topic
+
             self.destroy()
-            pass
-        else:
-            pass
 
     def cancel_callback(self):
-        self.destroy()
+        if tk.messagebox.askokcancel(parent = self,
+                                     title = "Cancel Session",
+                                     message = "Are you sure you want to cancel joining the CodeLive session?"):
+            self.data = None
+            self.destroy()
+    
+    def valid_name(self, s):
+        if len(s) < 8:
+            tk.messagebox.showerror(parent = self,
+                                     title = "Error",
+                                     message = "Please provide a name at least 8 characters long.")
+            return False
+        return True
+    
+    def valid_topic(self, s):
+        if len(s) < 12:
+            tk.messagebox.showerror(parent = self,
+                                     title = "Error",
+                                     message = "Please provide a unique topic with more than 12 characters.")
+            return False
+        
+        # TODO: replace with topic_exists(s) when topic_exists's logic is complete
+        if False: #topic_exists(s):
+            tk.messagebox.showerror(parent = self,
+                                     title = "Error",
+                                     message = "The topic doesn't exist. Make sure your topic is spelled correctly.")
+            return False
+        return True
 
 if __name__ == "__main__":
 
