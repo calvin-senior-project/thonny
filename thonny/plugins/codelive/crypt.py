efa43cb457ddbd9173389bd03111ff665de2a7f9
samigirma1 <sgz4>
2021-02-25 20:52:38 -0500

    Documentation and tests added to encryption module

--- thonny/plugins/codelive/crypt.py
+++ thonny/plugins/codelive/crypt.py
@@ -6,7 +6,13 @@ from binascii import unhexlify, b2a_base64
 
 GROUP = 16
 class Crypt:
+    '''
+    Crypt provides the high level encryption module that fits the purposes of Codelive
+    '''
     def __init__(self, pwd):
+        '''
+        pwd: string password
+        '''
         self._dh = pyDH.DiffieHellman(GROUP)
         
         self._pwd_key = Fernet.generate_key()
@@ -15,24 +21,37 @@ class Crypt:
 
         self._pwd_hash = Fernet(self._pwd_key).encrypt(bytes(pwd, "utf-8"))
 
-        
+        # used when the password, and diffie-hellman private keys are updated
         self._dh_lock = threading.Lock()
         self._pwd_lock = threading.Lock()
 
     def gen_handshake_pub(self):
+        '''
+        Generates the public key used for the key exchange
+        '''
         with self._dh_lock:
             return self._dh.gen_public_key()
 
     def get_handshake_shared(self, other_pub):
+        '''
+        Calculates the shared private key from the remote public key
+        '''
         with self._dh_lock:
             return self._dh.gen_shared_key(other_pub)
 
     def get_handshake_pwd_key(self, other_pub):
+        '''
+        Generates the password key encrypted by the shared private key 
+        calculated from the remote public key
+        '''
         key = b2a_base64(unhexlify(self._dh.gen_shared_key(other_pub)))
         with self._pwd_lock:
             return Fernet(key).encrypt(self._pwd_key)
 
     def get_session_key(self):
+        '''
+        Returns the session key
+        '''
         return self._sess_key
 
     def auth(self, _hash):
@@ -45,24 +64,36 @@ class Crypt:
         return ret
     
     def encrypt(self, plaintext, key = None):
+        '''
+        Encrypts plaintext using key (if provided) or the _sess_key
+        '''
         if key != None:
             return Fernet(key).encrypt(plaintext)
         
         return self._sess_crypt.encrypt(plaintext)
     
     def decrypt(self, ciphertext, key = None):
+        '''
+        Decrypts ciphertext using key (if provided) or the _sess_key
+        '''
         if key != None:
             return Fernet(key).decrypt(ciphertext)
         else:
             return self._sess_crypt.decrypt(ciphertext)
     
     def _update_pwd_key(self):
+        '''
+        Updates the password hash and key
+        '''
         with self._pwd_lock:
             pwd = Fernet(self._pwd_key).decrypt(self._pwd_hash)
             self._pwd_key = Fernet.generate_key()
             self._pwd_hash = Fernet(self._pwd_key).encrypt(pwd)
     
     def _replace_handshake_key(self):
+        '''
+        Updates the DiffieHellman key
+        '''
         with self._dh_lock:
             self._dh = pyDH.DiffieHellman(GROUP)
 
@@ -159,6 +190,27 @@ if __name__ == "__main__":
             
             print("\t Passed")
 
+        def _update_pwd_key(self):
+            print("- Testing _update_pwd_key... ", end = "")
+
+            c = Crypt("test_password")
+            c_hash = c._pwd_hash
+            c_key = c._pwd_key
+
+            c._update_pwd_key()
+
+            assert(c_hash != c._pwd_hash)
+            assert(c_key != c._pwd_key)
+            print("Passed")
+        
+        def _replace_handshake_key(self):
+            print("- Testing _replace_handshake_key... ", end = "")
+            c = Crypt("test_password")
+            c_dh = c._dh
+            c._replace_handshake_key()
+            assert(c._dh != c_dh)
+            print("Passed")
+
         def all(self):
             print("\nTesting Crypt...\n")
             self.consturctor()
@@ -169,6 +221,8 @@ if __name__ == "__main__":
             self.auth()
             self.encrypt()
             self.decrypt()
+            self._update_pwd_key()
+            self._replace_handshake_key()
             print("\nAll Tests passed!")
 
     test = CryptTester()
